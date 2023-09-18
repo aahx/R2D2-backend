187b5dba2f9b67585bd35671ecbc6a4c1056c899
Michael Kim <michaelkim009@gmail.com>
2023-09-17 20:55:15 -0700

    updated comments, cleaned up

--- main.py
+++ main.py
@@ -1,144 +1,115 @@
 import os
-import json
 from dotenv import load_dotenv
 
-# from langchain.document_loaders import TextLoader
-from langchain.document_loaders import JSONLoader
-from pathlib import Path
-from pprint import pprint
-
-
+from langchain.document_loaders import TextLoader
 from langchain.chains.summarize import load_summarize_chain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-# Accessing envs
+# Load environment variables
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 os.environ["OPENAI_API_KEY"] = API_KEY
 
-### Testing JSONLoader
-# json_file_path = './example_data/facebook_chat.json'
-# data = json.loads(Path(json_file_path).read_text())
-# pprint(data)
-# loader = JSONLoader(
-#     file_path='./example_data/facebook_chat.json',
-#     jq_schema='.messages[].content',
-#     text_content=False)
+# Define file paths for prospect and company information
+prospect_info_path = './example_data/prospect_info.txt'
+company_info_path = './example_data/company_info.txt'
 
-# data = loader.load()
-
-# pprint(data)
+# Read company information from a file
+with open(company_info_path, "r") as file:
+    company_info = file.read()
 
 
+# Load prospect information from a file
+def get_prospect_data():
+    loader = TextLoader(prospect_info_path)
+    return loader.load()
 
-# ################
-# #    Modular - loading in data
-# #   Can do Documentloader to load in multiple documents
-# #    * Look into Memory *
-# ################
-# def get_company_data():
-#     loader = TextLoader(prospect_info_path)
-#     return loader.load()
-
-# data = get_company_data()
-# # print(f"You have {len(data)} main document(s)")
-# # print(f"Preview of your data:\n\n{data[0].page_content[:80]}")
+data = get_prospect_data()
+print(f"You have {len(data)} main document(s)")
 
 
+# Split documents to avoid token limits and enable prompt engineering; Will be taking chunks and map_reducing
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap  = 0
+)
+
+docs = text_splitter.split_documents(data)
+print (f"You now have {len(docs)} split documents")
 
 
-# # Accessing txt files
-# prospect_info_path = "./data/prospect_info.txt"
-# company_info_path = "./data/company_info.txt"
-# with open(company_info_path, "r") as file:
-#     company_info = file.read()
+# Define map and combine prompts for text generation
+# Define map_prompt: This prompt is used during the initial processing of documents.
+# It instructs the model to generate concise summaries about the prospect from each document.
+# Placeholders like {text} and {prospect} will be replaced with actual content during text generation.
+map_prompt = """ 
+    % MAP PROMPT
+
+    Below is a section of a website about {prospect}
+
+    Write a concise summary about {prospect}. If the information is not about {prospect}, exclude it from your summary.
+
+    {text} 
+    """
+
+# Define prompt template for summarization of sections
+map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text", "prospect"])
 
 
+# Define combine_prompt: This prompt is used when combining the outputs of the map pass.
+# It guides the model to compose personalized outbound emails, incorporating information about both companies.
+# Placeholders like {company}, {company_information}, {sales_rep}, {prospect}, and {text} will be replaced during text generation.
+combine_prompt = """
+    % COMBINE PROMPT
+
+    Your goal is to write a personalized outbound email from {sales_rep}, a sales rep at {company} to {prospect}.
+
+    A good email is personalized and combines information about the two companies on how they can help each other.
+    Be sure to use value selling: A sales methodology that focuses on how your product or service will provide value to the customer instead of focusing on price or solution.
+
+    % INFORMATION ABOUT {company}:
+    {company_information}
+
+    % INFORMATION ABOUT {prospect}:
+    {text}
+
+    % INCLUDE THE FOLLOWING PIECES IN YOUR RESPONSE:
+        - Start the email with the sentence: "We love that {prospect} helps teams..." then insert what they help teams do.
+        - The sentence: "We can help you do XYZ by ABC" Replace XYZ with what {prospect} does and ABC with what {company} does 
+        - A 1-2 sentence description about {company}, be brief
+        - End your email with a call-to-action such as asking them to set up time to talk more 
+    """
+
+# Define prompt template for combined map_prompt output summarization
+combine_prompt_template = PromptTemplate(
+    template=combine_prompt, 
+    input_variables=["company", "company_information", "sales_rep", "prospect", "text"])
 
 
-# ################
-# #    Splitting up txt data to avoid token limits, especially
-# #    for lengthy docs
-# #    Don't really need at the moment
-# ################
-# text_splitter = RecursiveCharacterTextSplitter(
-#     # Set a really small chunk size, just to show.
-#     chunk_size = 800,
-#     chunk_overlap  = 0
-# )
-# docs = text_splitter.split_documents(data)
-# # print (f"You now have {len(docs)} split documents")
+# Initialize the OpenAI language model
+llm = OpenAI(temperature=.5)
 
+# Define a text generation chain
+# Even though we're not summarizing, we use the Summarize framework for efficient map-reduce processing.
+# Prompts (map_prompt_template and combine_prompt_template) provide specific directions for the text generation process.
+chain = load_summarize_chain(
+    llm,
+    chain_type="map_reduce",
+    map_prompt=map_prompt_template,
+    combine_prompt=combine_prompt_template,
+    verbose=True
+)
 
+# Generate text based on provided documents and prompts
+output = chain({
+    "input_documents": docs,
+    "company": "RapidRoad",
+    "company_information" : company_info,
+    "sales_rep" : "Michael",
+    "prospect" : "GitLab"
+})
 
-# ################
-# #    map_prompt will be the prompt done on the first pass of my documents
-# #    combine_prompt will be the propmt that is used when you combine the outputs of your map pass
-# ################
-# map_prompt = """ 
-# Below is a section of a website about {prospect}
-
-# Write a concise summary about {prospect}. If the information is not about {prospect}, exclude it from your summary.
-
-# {text}
-
-# % CONCISE SUMMARY : 
-# """
-
-# map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text", "prospect"])
-
-
-# combine_prompt = """
-# Your goal is to write a personalized outbound email from {sales_rep}, a sales rep at {company} to {prospect}.
-
-# A good email is personalized and combines information about the two companies on how they can help each other.
-# Be sure to use value selling: A sales methodology that focuses on how your product or service will provide value to 
-# the customer instead of focusing on price or solution.
-
-# % INFORMATION ABOUT {company}:
-#     {company_information}
-
-# % INFORMATION ABOUT {prospect}:
-#     {text}
-
-# % INCLUDE THE FOLLOWING PIECES IN YOUR RESPONSE:
-#     - Start the email with the sentence: "We love that {prospect} helps teams..." then insert what they help teams do.
-#     - The sentence: "We can help you do XYZ by ABC" Replace XYZ with what {prospect} does and ABC with what {company} does 
-#     - A 1-2 sentence description about {company}, be brief
-#     - End your email with a call-to-action such as asking them to set up time to talk more
-
-# YOUR RESPONSE: 
-# """
-
-# combine_prompt_template = PromptTemplate(
-#     template=combine_prompt, 
-#     input_variables=["company", "company_information", "sales_rep", "prospect", "text"])
-
-
-# llm = OpenAI(temperature=.5)
-# # llm = OpenAI(temperature=.5, openai_api_key=openai_api_key)
-
-# ################
-# #   Using Summarize to use map reduce in an efficient way, although I am not summarizing
-# #   By using prompts (map_prompt and combine_prompt), we are giving it more direction
-# ################
-# chain = load_summarize_chain(
-#     llm,
-#     chain_type="map_reduce",
-#     map_prompt=map_prompt_template,
-#     combine_prompt=combine_prompt_template,
-#     verbose=True
-# )
-
-# ################
-# output = chain({
-#     "input_documents": docs,
-#     "company": "RapidRoad",
-#     "company_information" : company_info,
-#     "sales_rep" : "Michael",
-#     "prospect" : "GitLab"
-# })
-
-# print(output['output_text'])
+# Print the generated output
+print(output['output_text'])
